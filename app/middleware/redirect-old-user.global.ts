ee95172bd60810b583eab5dfc3667e995f6a237a
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-02-18 13:17:39 +0100

    fix: remove query param after redirect

--- app/middleware/redirect-old-user.global.ts
+++ app/middleware/redirect-old-user.global.ts
@@ -2,6 +2,9 @@ export default defineNuxtRouteMiddleware((to) => {
   const user = to.query.user;
 
   if (user && to.path === "/") {
-    return navigateTo(`/user/${user}`, { redirectCode: 301, replace: true });
+    return navigateTo(
+      { path: `/user/${user}`, query: {} },
+      { redirectCode: 301, replace: true },
+    );
   }
 });
