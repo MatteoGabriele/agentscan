0aa47f3925e623479611a22992a2bdb9459be1c7
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-03-11 11:27:52 +0100

    feat: add bundled deps

--- .github/actions/agent-scan/tsdown.config.js
+++ .github/actions/agent-scan/tsdown.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./index.ts"],
+  format: ["cjs"],
+  outDir: "dist",
+  clean: true,
+  platform: "node",
+  target: "node24",
+  deps: {
+    alwaysBundle: ["@actions/core", "@actions/github"],
+  },
 });
