5e4c6b69f9e9caa3f1fead8144da3bc15cf82952
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-06-20 23:24:33 +0200

    feat: add bounty repo list

--- app/pages/lab.vue
+++ app/pages/lab.vue
@@ -88,6 +88,9 @@ const dataset = computed(() => stacklineData.value.dataset);
       <div class="w-full">
         <ChartGlobalEventsHeatmap :data="dataset" :timestamps="dates" />
       </div>
+      <div class="w-full">
+        <LabBountyRepoList />
+      </div>
     </div>
   </section>
 </template>
