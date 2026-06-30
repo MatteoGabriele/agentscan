6d91723f89d6423ffea56a0155bd17239fc01629
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-06-30 21:45:51 +0200

    feat: links active in child routes

--- app/components/Main/MenuItem.vue
+++ app/components/Main/MenuItem.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   label: string
   to: string
 }>()
+
+const route = useRoute()
+const isPathActive = computed(() => {
+  return props.to === '/' ? route.path === '/' : route.path.startsWith(props.to)
+})
 </script>
 
 <template>
   <NuxtLink
-    v-slot="{ isActive }"
     :to
     class="inline-flex items-center text-gh-muted hover:text-gh-text transition-colors text-sm"
   >
     <span
       class="text-gh-muted hover:text-gh-text transition-colors"
-      :class="{ 'text-gh-text': isActive }"
+      :class="{ 'text-gh-text': isPathActive }"
     >
       {{ label }}
     </span>
