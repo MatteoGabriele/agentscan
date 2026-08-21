431efcb4b837a7f551f1db0ffb98c987056598cd
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-08-22 00:10:29 +0200

    chore: remove used property

--- server/api/webhook/github.post.test.ts
+++ server/api/webhook/github.post.test.ts
@@ -65,7 +65,6 @@ const {
   vi.stubGlobal('readRawBody', readRawBodyMock)
   vi.stubGlobal('useRuntimeConfig', useRuntimeConfigMock)
   vi.stubGlobal('getHeader', getHeaderMock)
-  vi.stubGlobal('TrackedOctokit', vi.fn())
   vi.stubGlobal(
     'createError',
     ({ statusCode, message }: { statusCode: number; message: string }) => {
