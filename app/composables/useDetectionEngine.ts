4f6391b1a173fc28d1f23d3a340bd162e29f6759
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-09-05 14:56:53 +0200

    chore: shorter name for detection tag

--- app/composables/useDetectionEngine.ts
+++ app/composables/useDetectionEngine.ts
@@ -1,14 +1,13 @@
 import unveilIdentityPackage from '@unveil/identity/package.json'
 
-const repoName = 'unveil-project/identity'
 const version = unveilIdentityPackage.version
 
 export function useDetectionEngine() {
   return {
-    name: repoName,
+    name: 'identity',
     version,
     label: `v${version}`,
     isPrerelease: version.includes('-'),
-    npmUrl: `https://github.com/${repoName}`,
+    npmUrl: 'https://github.com/unveil-project/identity',
   }
 }
